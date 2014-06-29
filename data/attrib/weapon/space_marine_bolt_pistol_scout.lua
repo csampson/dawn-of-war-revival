868be7850716d90ff116a8ff3e13d3f836f50f9f
Cameron Sampson <vaerros@gmail.com>
2014-06-29 14:52:24 -0500

    Increased scout marine bolter damage and armor penetration
    Armor penetration is now consistent with all SM bolters

--- data/attrib/weapon/space_marine_bolt_pistol_scout.lua
+++ data/attrib/weapon/space_marine_bolt_pistol_scout.lua
@@ -1,23 +1,8 @@
-GameData = Inherit([[weapon\space_marine_bolt_pistol.lua]])
-MetaData = InheritMeta([[weapon\space_marine_bolt_pistol.lua]])
+GameData = Inherit([[weapon\space_marine_bolter.lua]])
+MetaData = InheritMeta([[weapon\space_marine_bolter.lua]])
 
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_01"]["armour_piercing_value"] = 2.30500
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_02"]["armour_piercing_value"] = 2.08300
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_04"]["armour_piercing_value"] = 27.00000
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_05"]["armour_piercing_value"] = 7.20000
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_07"]["armour_piercing_value"] = 50.00000
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_08"]["armour_piercing_value"] = 25.00000
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_09"]["armour_piercing_value"] = 54.00000
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_11"]["armour_piercing_value"] = 8.65400
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_12"]["armour_piercing_value"] = 61.76000
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_12"]["armour_type"] = Reference([[type_armour\tp_infantry_high.lua]])
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_13"]["armour_piercing_value"] = 56.78700
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_13"]["armour_type"] = Reference([[type_armour\tp_infantry_med.lua]])
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_14"]["armour_piercing_value"] = 60.78300
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_14"]["armour_type"] = Reference([[type_armour\tp_infantry_low.lua]])
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_piercing_value"] = 2.30500
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 12.90000
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 10.60000
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 11
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 8
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 1.00000
 
 
