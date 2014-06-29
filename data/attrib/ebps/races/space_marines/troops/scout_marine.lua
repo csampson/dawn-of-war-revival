cc45610e8adef44b0a6b32c8cce7aa8165600476
Cameron Sampson <vaerros@gmail.com>
2014-06-29 14:55:44 -0500

    Upgraded scout marine armor class and increased build time

--- data/attrib/ebps/races/space_marines/troops/scout_marine.lua
+++ data/attrib/ebps/races/space_marines/troops/scout_marine.lua
@@ -18,7 +18,7 @@ GameData["combat_ext"]["hardpoints"]["hardpoint_02"]["hardpoint_weapon_variant_m
 GameData["combat_ext"]["hardpoints"]["hardpoint_02"]["weapon_table"]["weapon_01"]["name_for_this_weapon_choice"] = "Space_Marines_Knife"
 GameData["combat_ext"]["hardpoints"]["hardpoint_02"]["weapon_table"]["weapon_01"]["weapon"] = "weapon\\space_marine_knife_pistol.lua"
 GameData["cost_ext"]["cost_req"] = 45.00000
-GameData["cost_ext"]["cost_time"] = 3.00000
+GameData["cost_ext"]["cost_time"] = 4.5
 GameData["cost_ext"]["time_cost"]["cost"]["requisition"] = 50.00000
 GameData["cost_ext"]["time_cost"]["time_seconds"] = 6.00000
 GameData["cover_ext"]["cover_heavy"]["modifiers"]["modifier_02"]["value"] = 1.00000
@@ -34,7 +34,7 @@ GameData["sight_ext"]["sight_radius"] = 30.00000
 GameData["special_attack_physics_ext"]["mass"] = 12.00000
 GameData["synckill_ext"]["synckillinfos"]["synckillinfo_01"]["killer_invulnerable_time"] = 5.63000
 GameData["synckill_ext"]["synckillinfos"]["synckillinfo_03"]["killer_invulnerable_time"] = 3.70000
-GameData["type_ext"]["type_armour"] = Reference([[type_armour\tp_infantry_med.lua]])
+GameData["type_ext"]["type_armour"] = Reference([[type_armour\tp_infantry_high.lua]])
 GameData["ui_ext"]["speech_directory"] = "Speech/Races/Space_Marines/Scout"
 GameData["ui_ext"]["ui_hotkey_name"] = "marine_scout"
 GameData["ui_ext"]["ui_index_hint"] = 2.00000
