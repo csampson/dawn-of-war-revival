f1359f7a2e54ed63e675adf8ccc4d27bcc895238
Cameron Sampson <vaerros@gmail.com>
2014-06-29 14:48:23 -0500

    Consoliated space marine knife armor penetration

--- data/attrib/weapon/space_marine_knife_veteran.lua
+++ data/attrib/weapon/space_marine_knife_veteran.lua
@@ -1,9 +1,6 @@
 GameData = Inherit([[weapon\space_marine_knife_tactical.lua]])
 MetaData = InheritMeta([[weapon\space_marine_knife_tactical.lua]])
 
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_09"]["armour_piercing_value"] = 50.88900
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_10"]["armour_piercing_value"] = 50.38300
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_11"]["armour_piercing_value"] = 50.00000
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 82.73100
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 50.96100
 
