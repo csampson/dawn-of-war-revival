a2a369fb7ec1f45c08ad9ee27a0764fabe4ccc34
Cameron Sampson <vaerros@gmail.com>
2014-07-06 02:13:17 -0500

    Made SM assault shotgun speed debuff non-stacking

--- data/attrib/weapon/space_marine_assault_shotgun.lua
+++ data/attrib/weapon/space_marine_assault_shotgun.lua
@@ -45,6 +45,7 @@ GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_01"]["max_lifeti
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_01"]["modifier"] = Reference([[modifiers\speed_maximum_modifier.lua]])
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_01"]["modifier"]["application_type"] = Reference([[type_modifierapplicationtype\tp_mod_apply_to_squad.lua]])
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_01"]["modifier"]["value"] = 0.7
+GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_01"]["modifier"]["exclusive"] = true
 GameData["cost"]["cost"]["power"] = 10.00000
 GameData["cost"]["cost"]["requisition"] = 30.00000
 GameData["cost"]["time_seconds"] = 11.00000
