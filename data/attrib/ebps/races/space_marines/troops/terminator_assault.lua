86ca73ee2b57e8518978bdd8e40336ef3499d080
Cameron Sampson <vaerros@gmail.com>
2014-07-09 00:08:49 -0500

    SM: updated assault termie model

--- data/attrib/ebps/races/space_marines/troops/terminator_assault.lua
+++ data/attrib/ebps/races/space_marines/troops/terminator_assault.lua
@@ -1,17 +1,17 @@
 GameData = Inherit([[ebps\races\space_marines\troops\terminator.lua]])
 MetaData = InheritMeta([[ebps\races\space_marines\troops\terminator.lua]])
 
-GameData["combat_ext"]["hardpoints"]["hardpoint_01"]["weapon_table"]["weapon_01"]["name_for_this_weapon_choice"] = ""
+GameData["combat_ext"]["hardpoints"]["hardpoint_01"]["weapon_table"]["weapon_01"]["name_for_this_weapon_choice"] = "<None>"
 GameData["combat_ext"]["hardpoints"]["hardpoint_01"]["weapon_table"]["weapon_01"]["weapon"] = ""
 GameData["combat_ext"]["hardpoints"]["hardpoint_01"]["weapon_table"]["weapon_02"]["name_for_this_weapon_choice"] = ""
 GameData["combat_ext"]["hardpoints"]["hardpoint_01"]["weapon_table"]["weapon_02"]["weapon"] = ""
 GameData["combat_ext"]["hardpoints"]["hardpoint_01"]["weapon_table"]["weapon_03"]["name_for_this_weapon_choice"] = ""
 GameData["combat_ext"]["hardpoints"]["hardpoint_01"]["weapon_table"]["weapon_03"]["weapon"] = ""
-GameData["combat_ext"]["hardpoints"]["hardpoint_02"]["weapon_table"]["weapon_01"]["name_for_this_weapon_choice"] = "thunder_hammer"
+GameData["combat_ext"]["hardpoints"]["hardpoint_02"]["weapon_table"]["weapon_01"]["name_for_this_weapon_choice"] = "space_marines_thunder_hammer"
 GameData["combat_ext"]["hardpoints"]["hardpoint_02"]["weapon_table"]["weapon_01"]["weapon"] = "weapon\\space_marine_thunder_hammer.lua"
 GameData["cost_ext"]["cost_req"] = 50.00000
 GameData["cost_ext"]["cost_time"] = 0.00000
-GameData["entity_blueprint_ext"]["animator"] = "Races/Space_Marines/Troops/Assault_Terminator"
+GameData["entity_blueprint_ext"]["animator"] = "Races/Space_Marines/Troops/terminator_codex"
 GameData["health_ext"]["hitpoints"] = 800.00000
 GameData["melee_ext"]["in_melee_modifiers"]["modifier_03"] = Reference([[modifiers\health_meleedamage_received_modifier.lua]])
 GameData["melee_ext"]["in_melee_modifiers"]["modifier_03"]["value"] = 0.90000
